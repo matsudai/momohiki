9dfd983106ac735cc16ed246e142eaf1e45c160b
matsudai <matsudai.social.1131@gmail.com>
2022-05-05 17:16:23 +0000

    Update configs for gh-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  /*
+   * アプリケーションのルートの設定です。
+   * Github Pagesで<username>.github.io/<basepath>によりホスティングされるため設定できるようにします。
+   */
+  ...(process.env.GH_BASE_PATH ? { basePath: process.env.GH_BASE_PATH, assetPrefix: process.env.GH_BASE_PATH } : {})
 };
 
 module.exports = nextConfig;
