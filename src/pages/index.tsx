781cac9cdcc14edabfa13d1255b66caab1d2fc30
matsudai <matsudai.social.1131@gmail.com>
2022-05-05 16:07:09 +0000

    Add download button
    * [TODO] Project download link
    * HTML donwload link
    * Markdown download link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
 import type { NextPage } from 'next';
-import { ClipboardEventHandler, createElement, DragEventHandler, SyntheticEvent, useEffect, useState } from 'react';
+import {
+  ClipboardEventHandler,
+  createElement,
+  DragEventHandler,
+  SyntheticEvent,
+  useEffect,
+  useRef,
+  useState
+} from 'react';
 import remarkGfm from 'remark-gfm';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
@@ -11,13 +19,17 @@ import Editor from '@monaco-editor/react';
 
 import 'highlight.js/styles/default.css';
 
+type EditorInstance = Parameters<Extract<Parameters<typeof Editor>[0]['onMount'], Function>>[0];
+
 const initialTextOnEditor = '# Hello World\n\nWelcome to my page 👀\n';
 
 const Home: NextPage = () => {
   const [textOnEditor, setTextOnEditor] = useState(initialTextOnEditor);
   const [Content, setContent] = useState(() => <div />);
-  const [hast, setHast] = useState<string>('');
+  const [htmlBody, setHtmlBody] = useState<string>('');
   const [mdastAsJson, setMdAstAsJson] = useState<string>('');
+  const editorRef = useRef<EditorInstance | null>(null);
+  const downloadLinkRef = useRef<HTMLAnchorElement | null>(null);
 
   useEffect(() => {
     const mdast = unified().use(remarkParse).use(remarkGfm).parse(textOnEditor);
@@ -36,7 +48,7 @@ const Home: NextPage = () => {
                 .use(rehypeReact, { createElement /* , components: { a: Ax } */ })
                 .stringify(transformedNode as any)
             );
-            setHast(
+            setHtmlBody(
               unified()
                 .use(rehypeStringify)
                 .stringify(transformedNode as any) as any
@@ -59,7 +71,25 @@ const Home: NextPage = () => {
         };
         ifs.onerror = reject;
       });
-      setTextOnEditor((text) => `${text}![${title}](${buffer})`);
+
+      const editor = editorRef.current;
+      if (editor != null) {
+        const selections = editor.getSelections();
+        if (selections != null) {
+          editor.executeEdits(
+            null,
+            selections.map((selection) => ({
+              range: selection,
+              text: `![${title}](${buffer})`,
+              forceMoveMarkers: true
+            }))
+          );
+        } else {
+          console.log('Cursor not found');
+        }
+      } else {
+        console.log('Editor not found');
+      }
     }
   };
 
@@ -83,9 +113,48 @@ const Home: NextPage = () => {
     event.stopPropagation();
   };
 
+  const downloadMarkdown = () => {
+    const link = downloadLinkRef.current;
+    if (link != null) {
+      const type = 'text/plain;charset=utf-8';
+      link.href = URL.createObjectURL(new Blob([textOnEditor], { type }));
+      link.download = 'index.md';
+      link.click();
+    } else {
+      console.log('Download link not found');
+    }
+  };
+
+  const downloadHtml = () => {
+    const link = downloadLinkRef.current;
+    if (link != null) {
+      const html = `<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8"><title>Document</title></head><body>${htmlBody}</body></html>`;
+      const type = 'text/html;charset=utf-8';
+      link.href = URL.createObjectURL(new Blob([html], { type }));
+      link.download = 'index.html';
+      link.click();
+    } else {
+      console.log('Download link not found');
+    }
+  };
+
   return (
     <div>
       <main>
+        <div>
+          <p style={{ backgroundColor: '#eeeeee' }}>Control</p>
+          <div style={{ display: 'flex' }}>
+            <button type="button" onClick={downloadMarkdown}>
+              Download Markdown
+            </button>
+            <button type="button" onClick={downloadHtml}>
+              Download HTML
+            </button>
+          </div>
+          <div style={{ display: 'none' }}>
+            <a ref={downloadLinkRef}></a>
+          </div>
+        </div>
         <div style={{ display: 'flex' }}>
           <div>
             <p style={{ backgroundColor: '#eeeeee' }}>Markdown</p>
@@ -97,38 +166,38 @@ const Home: NextPage = () => {
                 onChange={(value) => {
                   setTextOnEditor(value ?? '');
                 }}
-                width="640px"
-                height="360px"
+                width="45vw"
+                height="80vh"
+                onMount={(editor) => {
+                  editorRef.current = editor;
+                }}
               />
             </div>
           </div>
 
           <div>
             <p style={{ backgroundColor: '#eeeeee' }}>Rendered HTML</p>
-            <div>{Content}</div>
+            <div style={{ height: '80vh', width: '45vw', overflow: 'auto' }}>{Content}</div>
           </div>
         </div>
 
         <div style={{ display: 'flex' }}>
           <div>
             <p style={{ backgroundColor: '#eeeeee' }}>Markdown AST</p>
-            <Editor
-              defaultLanguage="json"
-              defaultValue=""
-              value={mdastAsJson}
-              onChange={(value) => {
-                setTextOnEditor(value ?? '');
-              }}
-              width="640px"
-              height="360px"
-            />
+            <div style={{ height: '360px', width: '640px', overflow: 'auto' }}>
+              <code>
+                <pre>{mdastAsJson}</pre>
+              </code>
+            </div>
           </div>
 
           <div>
             <p style={{ backgroundColor: '#eeeeee' }}>HTML AST</p>
-            <code>
-              <pre>{hast}</pre>
-            </code>
+            <div style={{ height: '360px', width: '640px', overflow: 'auto' }}>
+              <code>
+                <pre>{htmlBody}</pre>
+              </code>
+            </div>
           </div>
         </div>
       </main>
