0e5c44ee8337cf28b26619365d0d536b05aa4e22
matsudai <matsudai.social.1131@gmail.com>
2022-09-15 16:58:08 +0000

    Update preview to add padding (for list marker showing)

--- components/Preview.tsx
+++ components/Preview.tsx
@@ -4,5 +4,5 @@ import { useEditorState } from '../lib/storage';
 export const Preview: FC = () => {
   const [{ content }] = useEditorState();
 
-  return <div className="w-full h-full">{content}</div>;
+  return <div className="w-full h-full p-4">{content}</div>;
 };
